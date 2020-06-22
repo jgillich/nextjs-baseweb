b3214ecc7064c0daeb4ec74a78385ce4603a4d01
Jakob Gillich <jakob@gillich.me>
2020-06-22 19:10:07 +0200

    add toaster

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
 import {Button} from 'baseui/button';
 import {useStyletron} from 'baseui';
+import {toaster, ToasterContainer} from 'baseui/toast';
 
 export const sum = (a: number, b: number) => a + b;
 
 const Index: React.FC = () => {
   const [css, theme] = useStyletron();
   return (
-    <div>
-      <Button onClick={() => console.log('hey')}>Hello</Button>
-      <p className={css({color: theme.colors.accent600})}>Styled by hook</p>
-    </div>
+    <ToasterContainer>
+      <div>
+        <Button onClick={() => toaster.positive('ok', {})}>Hello</Button>
+        <p className={css({color: theme.colors.accent600})}>Styled by hook</p>
+      </div>
+    </ToasterContainer>
   );
 };
 
